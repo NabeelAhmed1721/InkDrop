e5a90883702b6c1e47f274b93bcfd6d11db86df9
Shajeel Ahmed <sharjeel4001@gmail.com>
2022-05-28 18:57:57 -0400

    deleted popup

--- src/popup.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import './popup.css';
-import Index from './popup/';
-
-const root = document.getElementById('popup');
-ReactDOM.render(<Index />, root);
--- src/popup/index.tsx
+++
@@ -1,9 +0,0 @@
-import style from './styles/index.module.css';
-
-export default function Index() {
-  return (
-    <div className={style.container}>
-      <h1>InkDrop</h1>
-    </div>
-  );
-}
