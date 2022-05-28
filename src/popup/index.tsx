5e120f89e0f4e31ee48584c5527157e7f8235404
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2022-05-28 16:10:13 -0400

    fixed name in popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,7 +3,7 @@ import style from './styles/index.module.css';
 export default function Index() {
   return (
     <div className={style.container}>
-      <h1>Auto-Citer</h1>
+      <h1>InkDrop</h1>
     </div>
   );
 }
